768b454c2ea9a6accb8ea9f1c522f840fd3a61cf
Twins-33 <shoulao33@gmail.com>
2021-01-03 21:24:17 +0530

    Akira 030121/0923p

--- app/src/main/java/com/example/hikersmatch/MainActivity.java
+++ app/src/main/java/com/example/hikersmatch/MainActivity.java
@@ -8,12 +8,17 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,11 +85,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateLocationToUser(Location location){
+        Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
         Log.i("LOCATION:",location.toString());
         latitude.setText(String.format(" LATITUDE:  %s", location.getLatitude()));
         longitude.setText(String.format(" LONGITUDE:  %s", location.getLongitude()));
         altitude.setText(String.format(" ALTITUDE:  %s", location.getAltitude()));
         accuracy.setText(String.format(" ACCURACY:  %s", location.getAccuracy()));
         String addressMessage = "No address found...    :(";
+        try {
+            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if(addressList!= null && addressList.size()>0){
+                addressMessage =  " ADDRESS:\n";
+                if(addressList.get(0).getThoroughfare() != null){
+                    addressMessage += addressList.get(0).getThoroughfare() + "\n";
+                }
+                if(addressList.get(0).getLocality() != null){
+                    addressMessage += addressList.get(0).getLocality() + "\n";
+                }
+                if(addressList.get(0).getPostalCode() != null){
+                    addressMessage += addressList.get(0).getPostalCode() + "\n";
+                }
+                if(addressList.get(0).getAdminArea() != null){
+                    addressMessage += addressList.get(0).getAdminArea() + "\n";
+                }
+                if(addressList.get(0).getCountryName() != null){
+                    addressMessage += addressList.get(0).getCountryName() + "\n";
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        address.setText(addressMessage);
     }
 }
